c077ff5bf58e29b8d1db3ba99680d5f1c2770b9e
mintaeh0 <mth1150@naver.com>
2024-02-03 00:30:03 +0900

    Firebase 연동 완료

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'bmi_page.dart';
 import 'diet_page.dart';
 import 'profile_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
