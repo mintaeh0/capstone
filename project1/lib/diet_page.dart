562488e7dac49a7a86d57085275921f199beefa0
mintaeh0 <mth1150@naver.com>
2024-02-02 16:41:44 +0900

    식단 추가 페이지 추가

--- project1/lib/diet_page.dart
+++ project1/lib/diet_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'diet_add_page.dart';
 
 // 식단 페이지
 class DietPage extends StatelessWidget {
@@ -27,94 +28,32 @@ class DietPage extends StatelessWidget {
               DataRow(cells: [DataCell(Text("지방")), DataCell(Text("50"))]),
               DataRow(cells: [DataCell(Text("칼로리")), DataCell(Text("2000"))]),
             ]),
-            Container(
-              child: Row(children: [
-                Text(" 아침 "),
-                Text(" 점심 "),
-                Text(" 저녁 "),
-                Text(" 간식 "),
-              ]),
-            ),
-            Container(
-              child: const SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(children: [
-                    Row(
-                      children: [
-                        Text(
-                          "밥(3)",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text("  "),
-                        Text("65 / 5 / 1 / 300"),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "된장찌개",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text("  "),
-                        Text("16 / 15 / 5 / 170"),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "된장찌개",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text("  "),
-                        Text("16 / 15 / 5 / 170"),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "된장찌개",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text("  "),
-                        Text("16 / 15 / 5 / 170"),
-                      ],
-                    ),
-                  ])),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true,
-                    builder: (context) {
-                      return Container(
-                        height: 500,
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        child: const Column(children: [
-                          TextField(
-                            decoration: InputDecoration(
-                                contentPadding:
-                                    EdgeInsets.all(double.minPositive),
-                                border: OutlineInputBorder(
-                                    borderSide: BorderSide()),
-                                hintText: "검색",
-                                prefixIcon: Icon(Icons.search)),
-                          )
-                        ]),
-                      );
-                    },
-                  );
-                },
-                child: const Text("편집/등록")),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const DietAddPage()));
+                  },
+                  child: Text("아침")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const DietAddPage()));
+                  },
+                  child: Text("점심")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const DietAddPage()));
+                  },
+                  child: Text("저녁")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const DietAddPage()));
+                  },
+                  child: Text("간식")),
+            ]),
           ],
         ));
   }
--- project1/lib/profile_page.dart
+++ project1/lib/profile_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:project1/bodyspec_set_page.dart';
 import 'proflie_set_page.dart';
 
 // 프로필 페이지
@@ -38,25 +37,6 @@ class ProfilePage extends StatelessWidget {
           ]),
         ),
       ),
-      GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => const BodyspecSetPage(),
-            ));
-          },
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(7),
-                color: Colors.grey[300]),
-            margin: EdgeInsets.only(left: 20, right: 20, bottom: 10),
-            padding: EdgeInsets.all(10),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("신체스펙 수정"),
-                  Icon(Icons.arrow_forward_ios, size: 15),
-                ]),
-          ))
     ]);
   }
 }
